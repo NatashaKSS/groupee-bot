a75e01e889fcfc225776c5a6bb825b167285c215
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-08-09 20:13:43 +0800

    Configured custom winston logger

--- service.js
+++ service.js
@@ -1,3 +1,24 @@
+let logger = require('winston');
+
+const { createLogger, format, transports } = logger;
+
+logger = createLogger({
+  format: format.combine(
+    format.label({ label: '[Serve]' }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:ss:mm' }),
+    format.splat(),
+    format.printf(info => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`),
+  ),
+  transports: [
+    new transports.Console({
+      level: 'error',
+      eol: '\n',
+      handleExceptions: true,
+      colorize: true,
+    }),
+  ],
+});
+
 module.exports.serve = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -6,7 +27,8 @@ module.exports.serve = (event, context, callback) => {
     }),
   };
 
-  console.log(JSON.parse(event.body));
+  logger.debug('winston got a debug');
+  logger.error('winston got a error %o', { hello: 'world' });
 
   callback(null, response);
 };
