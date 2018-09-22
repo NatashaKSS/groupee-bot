6a95397f8087ab4bbe93070906815222d7d82a3c
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-09-22 22:47:37 +0800

    Add basic service handler for text & image telegram messages

--- service.js
+++ service.js
@@ -1,15 +1,40 @@
+import isEmpty from 'lodash/fp/isEmpty';
+import { TelegramClient } from 'messaging-api-telegram';
+
+// Setup logs
 require('./src/configs/logConfig');
 
-module.exports.serve = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: JSON.parse(event.body),
-    }),
-  };
+module.exports.serve = async (event, context, callback) => {
+  const client = TelegramClient.connect(process.env.TELEGRAM_TOKEN);
 
-  logger.debug('winston got a debug');
-  logger.error('winston got a error %o', { hello: 'world' });
+  if (!isEmpty(event.body)) {
+    const data = JSON.parse(event.body);
+    logger.debug(`[Data] ${JSON.stringify(data)}`);
 
-  callback(null, response);
+    if (!isEmpty(data.message)) {
+      const messageObj = data.message;
+      const { text, photo, chat } = messageObj;
+
+      if (typeof messageObj !== 'undefined') {
+        if (text) {
+          logger.debug(`[Sent] ${text}`);
+          client.sendMessage(chat.id, JSON.stringify(text)).then(() => {
+            logger.debug(`[Sent] ${text}`);
+            callback(null, { statusCode: 200 });
+          });
+        } else if (photo) {
+          callback(null, { statusCode: 200 });
+          client.sendMessage(chat.id, 'Received your photo!').then(() => {
+            logger.debug(`[Sent] ${text}`);
+            callback(null, { statusCode: 200 });
+          });
+          // client.sendMessage(chat.id, JSON.stringify(OCRResultObj)).then(() => {
+          //   callback(null, { statusCode: 200 });
+          // });
+        }
+      }
+    }
+  }
+
+  callback(null, { statusCode: 200 });
 };
