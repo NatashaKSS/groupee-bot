7d7476fc9ad2a1778495bd74e00e870989c3271b
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-09-24 21:56:46 +0800

    Simplify main lambda handler

--- service.js
+++ service.js
@@ -1,40 +1,18 @@
 import isEmpty from 'lodash/fp/isEmpty';
-import { TelegramClient } from 'messaging-api-telegram';
+import handleMessage from './src/app/telegram/telegram-message-handler';
 
 // Setup logs
 require('./src/config/logConfig');
 
 module.exports.serve = async (event, context, callback) => {
-  const client = TelegramClient.connect(process.env.TELEGRAM_TOKEN);
-
   if (!isEmpty(event.body)) {
     const data = JSON.parse(event.body);
+
     logger.debug(`[Data] ${JSON.stringify(data)}`);
 
     if (!isEmpty(data.message)) {
-      const messageObj = data.message;
-      const { text, photo, chat } = messageObj;
-
-      if (typeof messageObj !== 'undefined') {
-        if (text) {
-          logger.debug(`[Sent] ${text}`);
-          client.sendMessage(chat.id, JSON.stringify(text)).then(() => {
-            logger.debug(`[Sent] ${text}`);
-            callback(null, { statusCode: 200 });
-          });
-        } else if (photo) {
-          callback(null, { statusCode: 200 });
-          client.sendMessage(chat.id, 'Received your photo!').then(() => {
-            logger.debug(`[Sent] ${text}`);
-            callback(null, { statusCode: 200 });
-          });
-          // client.sendMessage(chat.id, JSON.stringify(OCRResultObj)).then(() => {
-          //   callback(null, { statusCode: 200 });
-          // });
-        }
-      }
+      handleMessage(data.message);
     }
   }
-
   callback(null, { statusCode: 200 });
 };
